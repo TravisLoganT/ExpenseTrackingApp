001413924db744bc51329d4a0c95655022efdddf
Travis <travislogant@gmail.com>
2024-03-18 23:27:13 +0000

    added function layout for app

--- expense_tracker.py
+++ expense_tracker.py
@@ -2,11 +2,30 @@ def main():
     print("Hello World")
     
     #TODO Get user input for expense
+    get_user_expense()
     
     #TODO Write their expense to a CSV file
+    save_expense_to_file()
     
     #TODO Read file and summarise expense
-    pass
+    read_expense_file()
+    
+
+
+def get_user_expense():
+    print("Getting user expense...")
+    
+
+
+def save_expense_to_file():
+    print("Saving user Expense...")
+    
+
+
+def read_expense_file():
+    print("Reading Expense File...")
+    
+
 
 if __name__ == "__main__":
     main()
