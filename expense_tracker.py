632b4c7ef65268b3dd99cc7e16120ddaecd323be
Travis <travislogant@gmail.com>
2024-03-18 23:20:18 +0000

    created outline for project setup

---
+++ expense_tracker.py
@@ -0,0 +1,12 @@
+def main():
+    print("Hello World")
+    
+    #TODO Get user input for expense
+    
+    #TODO Write their expense to a CSV file
+    
+    #TODO Read file and summarise expense
+    pass
+
+if __name__ == "__main__":
+    main()
